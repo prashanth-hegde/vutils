03467ff8730ba0720933ce6842e94eba0ccb24f6
Prashanth.Hegde <Prashanth.Hegde@target.com>
2024-03-30 18:27:07 -0500

    fix(build): Updating v to use absolute path for builds

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ cd ..
 
 # Build the projects
 mkdir bin 
-v -prod -o bin/epoch src/utils/epoch.v
-v -prod -o bin/loop src/utils/loop.v
-v -prod -o bin/urlencode src/utils/urlencode.v
-v -prod -o bin/media src/media/media.v
-v -prod -o bin/update src/update
+v -prod -o bin/epoch $PWD/src/utils/epoch.v
+v -prod -o bin/loop $PWD/src/utils/loop.v
+v -prod -o bin/urlencode $PWD/src/utils/urlencode.v
+v -prod -o bin/media $PWD/src/media/media.v
+v -prod -o bin/update $PWD/src/update
 
 tar -czvf bin.tgz bin/*
