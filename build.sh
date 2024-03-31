0879233dbc2363aa7198eeeb61f528c51cf28b5b
Prashanth.Hegde <Prashanth.Hegde@target.com>
2024-03-30 23:51:08 -0500

    cicd: update build script to be more efficient

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
-
-git clone --depth 1 --branch master https://github.com/vlang/v.git
-cd v 
-make 
-./v symlink
-cd ..
+# if command v is not present 
+if ! command -v v > /dev/null 2>&1; then
+  git clone --depth 1 --branch master https://github.com/vlang/v.git
+  cd v 
+  make 
+  ./v symlink
+  cd ..
+fi
 
 # Build the projects
-mkdir bin 
+# if directory bin is not present 
+if [ ! -d bin ]; then
+  mkdir bin
+fi
 v -prod -o bin/epoch $PWD/src/utils/epoch.v
 v -prod -o bin/loop $PWD/src/utils/loop.v
 v -prod -o bin/urlencode $PWD/src/utils/urlencode.v
