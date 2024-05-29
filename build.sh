de3bee19c065adacee558c69583ea714c5f64d24
Prashanth.Hegde <Prashanth.Hegde@target.com>
2024-05-29 06:51:43 -0500

    static binaries

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-# set -x 
+# set -x
 
-# if command v is not present 
+# if command v is not present
 if ! command -v v > /dev/null 2>&1; then
   git clone --depth 1 --branch master https://github.com/vlang/v.git
-  cd v 
-  make 
+  cd v
+  make
   ./v symlink
   cd ..
 fi
@@ -13,12 +13,12 @@ fi
 if [ ! -d bin ]; then
   mkdir bin
 fi
-v -prod -o bin/epoch $PWD/src/utils/epoch.v
-v -prod -o bin/loop $PWD/src/utils/loop.v
-v -prod -o bin/urlencode $PWD/src/utils/urlencode.v
-v -prod -o bin/media $PWD/src/media/media.v
-v -prod -o bin/update $PWD/src/update
-v -prod -o bin/paths $PWD/src/utils/paths.v
-v -prod -o bin/filecache $PWD/src/filecache
+v -prod -skip-unused -compress -cflags -static -o bin/urlencode $PWD/src/utils/urlencode.v
+v -prod -skip-unused -compress -cflags -static -o bin/epoch $PWD/src/utils/epoch.v
+v -prod -skip-unused -compress -cflags -static -o bin/loop $PWD/src/utils/loop.v
+v -prod -skip-unused -compress -cflags -static -o bin/media $PWD/src/media/media.v
+v -prod -skip-unused -compress -cflags -static -o bin/update $PWD/src/update
+v -prod -skip-unused -compress -cflags -static -o bin/paths $PWD/src/utils/paths.v
+v -prod -skip-unused -compress -cflags -static -o bin/filecache $PWD/src/filecache
 
 tar -czvf bin.tgz bin/*
