c600e2ce6be757472fcbf98de76c128dfabda0df
Prashanth.Hegde <Prashanth.Hegde@target.com>
2024-05-29 07:50:12 -0500

    static binaries for all os

--- build.sh
+++ build.sh
@@ -13,12 +13,20 @@ fi
 if [ ! -d bin ]; then
   mkdir bin
 fi
-v -prod -skip-unused -compress -cflags -static -o bin/urlencode $PWD/src/utils/urlencode.v
-v -prod -skip-unused -compress -cflags -static -o bin/epoch $PWD/src/utils/epoch.v
-v -prod -skip-unused -compress -cflags -static -o bin/loop $PWD/src/utils/loop.v
-v -prod -skip-unused -compress -cflags -static -o bin/media $PWD/src/media/media.v
-v -prod -skip-unused -compress -cflags -static -o bin/update $PWD/src/update
-v -prod -skip-unused -compress -cflags -static -o bin/paths $PWD/src/utils/paths.v
-v -prod -skip-unused -compress -cflags -static -o bin/filecache $PWD/src/filecache
+
+# configure compile flags
+CFLAGS="-prod -skip-unused -compress"
+# if linux then add -cflags -static
+if [ "$(uname)" == "Linux" ]; then
+	CFLAGS="$CFLAGS -cflags -static"
+fi
+
+v ${CFLAGS} -o bin/urlencode $PWD/src/utils/urlencode.v
+v ${CFLAGS} -o bin/epoch $PWD/src/utils/epoch.v
+v ${CFLAGS} -o bin/loop $PWD/src/utils/loop.v
+v ${CFLAGS} -o bin/media $PWD/src/media/media.v
+v ${CFLAGS} -o bin/update $PWD/src/update
+v ${CFLAGS} -o bin/paths $PWD/src/utils/paths.v
+v ${CFLAGS} -o bin/filecache $PWD/src/filecache
 
 tar -czvf bin.tgz bin/*
